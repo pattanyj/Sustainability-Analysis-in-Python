69d9ea240c26bdd3b8f9bbbca40845d1082337df
Jasmine Pattany <pattanyj@gmail.comcd>
2021-10-05 14:33:09 +0200

    Commits week 2 python

--- assignment_Jas.py
+++ assignment_Jas.py
@@ -9,15 +9,14 @@ import numpy as np
 import pandas as pd
 import os
 
-# check path
-os.chdir(r"C:\Users\patta\OneDrive\Desktop\Sustainability Analysis in Python\Assignment")
+# # check path
+os.chdir(r"C:\Users\patta\Sustainability Analysis in Python\Assignment\Sustainability-Analysis-in-Python")
 print("Current Working Directory " , os.getcwd())
 
 # read data
-df = pd.read_excel('Goal1.xlsx', sheet_name='data')
+df = pd.read_csv('Goal1.csv')
 
 #%% filter the data
-# print(df.isna().sum())
 # drop nan columns
 df = df.drop(['TimeCoverage', 'UpperBound', 'LowerBound','BasePeriod','GeoInfoUrl','FootNote'], axis=1)
 
@@ -25,13 +24,17 @@ df = df.drop(['TimeCoverage', 'UpperBound', 'LowerBound','BasePeriod','GeoInfoUr
 countries = df['GeoAreaName'].to_list()
 unique_countries = pd.unique(countries)
 
-#calculate nan values for each country
-for i in range(len(df['GeoAreaName'])):
-    if unique_countries == df['GeoAreaName']:
-        np.count(df['Value'])
-        min(df['Value'])
-        max(df['Value'])
-        np.average(df['Value'])
+#%% sanity checks for each country
+for country in unique_countries:
+    subset = df[df['GeoAreaName'] == country]
+    minimum=subset['Value'].min()
+    maximum=subset['Value'].max()
+    avg=subset['Value'].mean()
+    print(country," : ", 
+          'The minimum value is', minimum,
+          'The maximum value is', maximum,
+          'The average value is', avg)
+    
 
 
 
