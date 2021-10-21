599ada5169a1b05058e0484454f8b1741193148d
Jasmine Pattany <pattanyj@gmail.comcd>
2021-10-21 14:33:13 +0200

    changes- pbias done!

--- assignment_Jas.py
+++ assignment_Jas.py
@@ -8,7 +8,7 @@ Created on Thu Sep 23 14:46:32 2021
 import numpy as np
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 # # check path
@@ -26,16 +26,14 @@ df = df[df.columns.drop(list(df.filter(regex='Unnamed')))]
 # focusing on women between 15-24 only 
 df_female = df.loc[df["Sex"]=="FEMALE"]
 df_final = df_female.loc[df_female["Age"]=="15-24"]
- 
 
-#%%
 #country list
-countries = df_female['GeoAreaName'].to_list()
+countries = df_final['GeoAreaName'].to_list()
 unique_countries = pd.unique(countries)
 
 #%% sanity checks for each country
 for country in unique_countries:
-    subset = df_female[df_female['GeoAreaName'] == country]
+    subset = df_final[df_final['GeoAreaName'] == country]
     minimum=subset['Value'].min()
     maximum=subset['Value'].max()
     avg=subset['Value'].mean()
@@ -108,6 +106,9 @@ def calibration(x, y):
                         y[slope.index(max(slope))+1]])/2)
         start = max(y) - K
     
+    if start <0:
+        start = 0
+        
     # curve fitting
     popt, _ = curve_fit(logistic, x, y, p0 = [start, K, x_peak, 0], maxfev = 10000,
                         bounds = ([0.5*start, 0.5*K, 1995, -10],
@@ -115,12 +116,116 @@ def calibration(x, y):
     # +0.001 so that upper bound always larger than lower bound even if start=0
     return popt
 
-#%%
+# Using calibration and logistic functions
 popt_list=[]
+logistic_values=[]
 
 for country in unique_countries:
-    subset = df_female[df_female['GeoAreaName'] == country]
+    subset = df_final[df_final['GeoAreaName'] == country]
     year = subset['TimePeriod']
     value = subset['Value']
     temp_popt = calibration(year, value)
     popt_list.append(temp_popt)
+    log_list = logistic(year,temp_popt[0],temp_popt[1],temp_popt[2],temp_popt[3])
+    logistic_values.append(log_list)
+    
+    
+#%% Evaluate the model using R2
+from sklearn.metrics import r2_score, mean_squared_error
+
+y_true = df_final['Value'].to_list()
+
+# unpack the y_pred values from the log function with a nested for loop
+y_pred = []
+for value in logistic_values:
+    for item in value:
+        y_pred.append(item)
+ 
+#calculate r2
+r2_value = r2_score(y_true, y_pred)
+
+#NRMSE
+#calculate RMSE
+rmse=mean_squared_error(y_true, y_pred)
+avg_y_true = sum(y_true)/len(y_true)
+nrmse = rmse/avg_y_true
+
+#%%PBIAS
+#create new df to seperate by country
+data ={'Countries':countries, 'Years':df_final['TimePeriod'],  'Y_true':y_true,'Y_pred':y_pred}
+pbias_df = pd.DataFrame(data)
+# pbias_df=pbias_df.groupby(Countries)      #this doesnt work
+
+pbias_values=[]
+for country in unique_countries:
+    subset = pbias_df[pbias_df['Countries'] == country]
+    y_true_1 = subset['Y_true']
+    y_pred_1 = subset['Y_pred']
+    y_true_1= np.array(y_true_1)
+    y_pred_1=np.array(y_pred_1)
+    pbias = 100* ((sum(y_pred_1-y_true_1))/sum(y_pred_1))
+    pbias_values.append(pbias)
+
+#%% 
+import numpy as np
+from sklearn.model_selection import KFold
+X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
+y = np.array([1, 2, 3, 4])
+kf = KFold(n_splits=2)
+kf.get_n_splits(X)
+
+print(kf)
+
+for train_index, test_index in kf.split(X):
+print("TRAIN:", train_index, "TEST:", test_index)
+X_train, X_test = X[train_index], X[test_index]
+y_train, y_test = y[train_index], y[test_index]
+
+#%% Button
+
+'Still working on this, it runs but not connected to code'
+
+import sys
+from PyQt5.QtWidgets import (QLabel, QRadioButton, QPushButton, QVBoxLayout, QApplication, QWidget)
+
+
+class basicRadiobuttonExample(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.label = QLabel('Select the cross-validation method')
+        self.rbtn1 = QRadioButton('5-fold cross validation')
+        self.rbtn2 = QRadioButton('10-fold cross validation')
+        self.label2 = QLabel("")
+        
+        # self.rbtn1.toggled.connect(self.onClicked)
+        # self.rbtn2.toggled.connect(self.onClicked)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.rbtn1)
+        layout.addWidget(self.rbtn2)
+        layout.addWidget(self.label2)
+        
+        self.setGeometry(600, 600,900, 50)
+        # self.setFont(QFont('Arial', 10)
+        self.setLayout(layout)
+        self.setWindowTitle('Input Needed')
+        
+
+        self.show()
+
+    # def onClicked(self):
+    #     radioBtn = self.sender()
+    #     if radioBtn.isChecked():
+    #         self.label2.setText("You live in " + radioBtn.text())
+
+
+if __name__ == '__main__':    
+    app = QApplication(sys.argv)
+    ex = basicRadiobuttonExample()
+    sys.exit(app.exec_())
